ec2bec8ce1131f52e76910d307bcd5a465576443
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-20 10:31:27 +0200

    feat: add more rules

--- mocha/index.js
+++ mocha/index.js
@@ -23,6 +23,7 @@ module.exports = {
         "chai-expect/no-inner-compare": "error",
         "chai-expect/no-inner-literal": "error",
         "chai-expect/missing-assertion": "error",
-        "chai-expect/terminating-properties": "error"
+        "chai-expect/terminating-properties": "error",
+        "chai-friendly/no-unused-expressions": "error"
     }
 }
