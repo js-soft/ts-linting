1a8c093693ab72222202b8055fba8ec17f542353
jkoenig <julian.koenig@js-soft.com>
2021-09-20 10:45:54 +0200

    chore: add chai-friendly

--- mocha/index.js
+++ mocha/index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = {
-    plugins: ["mocha", "jest", "chai-expect"],
+    plugins: ["mocha", "jest", "chai-expect", "chai-friendly"],
     reportUnusedDisableDirectives: true,
     rules: {
         "mocha/no-async-describe": "error",
@@ -24,6 +24,7 @@ module.exports = {
         "chai-expect/no-inner-literal": "error",
         "chai-expect/missing-assertion": "error",
         "chai-expect/terminating-properties": "error",
+        "no-unused-expressions": "off",
         "chai-friendly/no-unused-expressions": "error"
     }
 }
