247c64f7d1a046e7bb40b6fcc2ebc93719b5d29d
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-06-25 13:59:20 +0200

    Chore/typescript 5.5 support (#25)
    * chore: add ts 5.5 support
    
    * chore: version bump
    
    * fix: update rules

--- jest/index.js
+++ jest/index.js
@@ -17,7 +17,7 @@ module.exports = {
         "jest/no-export": "error",
         "jest/no-focused-tests": "error",
         "jest/no-identical-title": "error",
-        "jest/no-if": "error",
+        "jest/no-conditional-in-test": "error",
         "jest/no-mocks-import": "error",
         "jest/no-standalone-expect": "error",
         "jest/no-test-prefixes": "error",
