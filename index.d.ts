dad6647b0f71ccb20de7031a0245856445a413f9
Julian König <julian.koenig@js-soft.com>
2025-07-15 11:56:37 +0200

    fix: add d.ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import { ConfigArray } from "typescript-eslint"
+
+declare const base: ConfigArray
+declare const jest: ConfigArray
+declare const withFormatting: ConfigArray
+
+declare const base: ConfigArray
+export default base
+
+export { base, jest, withFormatting }
